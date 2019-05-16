d9cef41f5f78e2aa177e1fc3bab8606395a8f54e
Austin Medeiros <austin.medeiros@jmir.org>
2019-05-15 23:26:13 -0400

    Added logging method and adjusted variable names for file.

--- cpuMonitor.c
+++ cpuMonitor.c
@@ -1,110 +1,134 @@
 #include <stdio.h> // printf, sprintf
 #include <stdlib.h> // exit, atoi, malloc, free
 #include <time.h> // nanosleep
-#include <string.h> // strcpy
 #include <unistd.h> // read, write, close
-#include <string.h> // memcpy, memset
+#include <string.h> // memcpy, memset, strcpy
 #include <sys/socket.h> // socket, connect
 #include <netinet/in.h> // struct sockaddr_in, struct sockaddr
 #include <netdb.h> // struct hostent, gethostbyname
 
 #define milSec 500
 
-void error(const char *msg) {
-        perror(msg); exit(0);
+void Log(const char *message, const int error)
+{
+        FILE *file;
+        char filePath[1000];
+
+        if(!error) {
+            printf("%s\n", "not an error");
+            strcpy(filePath, "/var/log/daemon_log");
+        } else {
+            strcpy(filePath, "/var/log/daemon_error");
+        }
+
+        file = fopen(filePath, "ab+");
+
+        if (file == NULL) {
+                /* Unable to open file hence exit */
+                printf("\nUnable to open '%s' file.\n", filePath);
+                printf("Please check whether file exists and you have write privilege.\n");
+                exit(EXIT_FAILURE);
+        }
+
+        fputs(message, file);
+
+        fclose(file);
+
+        if(error) exit(1);
+
 }
 
 void request(const char *body)
 {
-    int i;
+        int i;
 
-    /* first where are we going to send it? */
-    int port = 80;
-    char *host = "localhost";
+        // first where are we going to send it?
+        int port = 80;
+        char *host = "localhost";
 
-    struct hostent *server;
-    struct sockaddr_in serv_addr;
-    int sockfd, bytes, sent, received, total, message_size;
-    char *message, response[4096];
+        struct hostent *server;
+        struct sockaddr_in serv_addr;
+        int sockfd, bytes, sent, received, total, message_size;
+        char *message, response[4096];
 
-    /* How big is the message? */
-    message_size=0;
-    message_size+=strlen("POST / HTTP/1.0\r\n");
-    message_size+=strlen("Content-Type: application/json");
-    message_size+=strlen("\r\n");
-    message_size+=strlen("Content-Length: \r\n")+strlen(body);
-    message_size+=strlen("\r\n");
-    message_size+=strlen(body);
-
-
-    /* allocate space for the message */
-    message=malloc(message_size);
-
-    sprintf(message,"POST / HTTP/1.0\r\n");
-    strcat(message,"Content-Type: application/json");
-    strcat(message,"\r\n");
-    sprintf(message+strlen(message), "Content-Length: %ld\r\n", strlen(body));
-    strcat(message,"\r\n");
-
-    strcat(message,body);
+        // How big is the message?
+        message_size=0;
+        message_size+=strlen("POST / HTTP/1.0\r\n");
+        message_size+=strlen("Content-Type: application/json");
+        message_size+=strlen("\r\n");
+        message_size+=strlen("Content-Length: \r\n")+strlen(body);
+        message_size+=strlen("\r\n");
+        message_size+=strlen(body);
 
 
-    /* What are we going to send? */
-    printf("Request:\n%s\n",message);
+        // allocate space for the message
+        message=malloc(message_size);
 
-    /* create the socket */
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0) error("ERROR opening socket");
+        sprintf(message,"POST / HTTP/1.0\r\n");
+        strcat(message,"Content-Type: application/json");
+        strcat(message,"\r\n");
+        sprintf(message+strlen(message), "Content-Length: %ld\r\n", strlen(body));
+        strcat(message,"\r\n");
 
-    /* lookup the ip address */
-    server = gethostbyname(host);
-    if (server == NULL) error("ERROR, no such host");
+        strcat(message,body);
 
-    /* fill in the structure */
-    memset(&serv_addr,0,sizeof(serv_addr));
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(port);
-    memcpy(&serv_addr.sin_addr.s_addr,server->h_addr,server->h_length);
 
-    /* connect the socket */
-    if (connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0)
-            error("ERROR connecting");
+        // What are we going to send?
+        printf("Request:\n%s\n",message);
 
-    /* send the request */
-    total = strlen(message);
-    sent = 0;
-    do {
-            bytes = write(sockfd,message+sent,total-sent);
-            if (bytes < 0)
-                    error("ERROR writing message to socket");
-            if (bytes == 0)
-                    break;
-            sent+=bytes;
-    } while (sent < total);
+        // create the socket
+        sockfd = socket(AF_INET, SOCK_STREAM, 0);
+        if (sockfd < 0) Log("ERROR opening socket", 1);
 
-    /* receive the response */
-    memset(response,0,sizeof(response));
-    total = sizeof(response)-1;
-    received = 0;
-    do {
-            bytes = read(sockfd,response+received,total-received);
-            if (bytes < 0)
-                    error("ERROR reading response from socket");
-            if (bytes == 0)
-                    break;
-            received+=bytes;
-    } while (received < total);
+        // lookup the ip address
+        server = gethostbyname(host);
+        if (server == NULL) Log("ERROR, no such host", 1);
 
-    if (received == total)
-            error("ERROR storing complete response from socket");
+        // fill in the structure
+        memset(&serv_addr,0,sizeof(serv_addr));
+        serv_addr.sin_family = AF_INET;
+        serv_addr.sin_port = htons(port);
+        memcpy(&serv_addr.sin_addr.s_addr,server->h_addr,server->h_length);
 
-    /* close the socket */
-    close(sockfd);
+        // connect the socket
+        if (connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0)
+                Log("ERROR connecting", 1);
 
-    /* process response */
-    printf("Response:\n%s\n",response);
+        // send the request
+        total = strlen(message);
+        sent = 0;
+        do {
+                bytes = write(sockfd,message+sent,total-sent);
+                if (bytes < 0)
+                        Log("ERROR writing message to socket", 1);
+                if (bytes == 0)
+                        break;
+                sent+=bytes;
+        } while (sent < total);
 
-    free(message);
+        // receive the response
+        memset(response,0,sizeof(response));
+        total = sizeof(response)-1;
+        received = 0;
+        do {
+                bytes = read(sockfd,response+received,total-received);
+                if (bytes < 0)
+                        Log("ERROR reading response from socket", 1);
+                if (bytes == 0)
+                        break;
+                received+=bytes;
+        } while (received < total);
+
+        if (received == total)
+                Log("ERROR storing complete response from socket", 1);
+
+        // close the socket
+        close(sockfd);
+
+        // process response
+        printf("Response:\n%s\n",response);
+
+        free(message);
 }
 
 int main()
@@ -112,7 +136,7 @@ int main()
         char body[256];
         char loadAvgString[256];
         long double a[4], b[4], loadavg;
-        FILE *fp;
+        FILE *file;
 
         struct timespec ts;
         ts.tv_sec = milSec / 1000;
@@ -120,14 +144,14 @@ int main()
 
         for(;;)
         {
-                fp = fopen("/proc/stat","r");
-                fscanf(fp,"%*s %Lf %Lf %Lf %Lf",&a[0],&a[1],&a[2],&a[3]);
-                fclose(fp);
+                file = fopen("/proc/stat","r");
+                fscanf(file,"%*s %Lf %Lf %Lf %Lf",&a[0],&a[1],&a[2],&a[3]);
+                fclose(file);
                 nanosleep(&ts, NULL);
 
-                fp = fopen("/proc/stat","r");
-                fscanf(fp,"%*s %Lf %Lf %Lf %Lf",&b[0],&b[1],&b[2],&b[3]);
-                fclose(fp);
+                file = fopen("/proc/stat","r");
+                fscanf(file,"%*s %Lf %Lf %Lf %Lf",&b[0],&b[1],&b[2],&b[3]);
+                fclose(file);
 
                 loadavg = ((b[0]+b[1]+b[2]) - (a[0]+a[1]+a[2])) / ((b[0]+b[1]+b[2]+b[3]) - (a[0]+a[1]+a[2]+a[3])) * 100;
                 sprintf(loadAvgString, "%.2Lf", loadavg);
